270ab69bbb658af7b5156082bac256cd3c5852fe
QuangHuy301 <lqhuy301@gmail.com>
2024-10-19 00:06:15 +0700

    4.create header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Header from "./Components/Header";
+
 
 function App() {
   return (
-    <>
-      <h1 className='text-danger'>Hello world !! </h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" exact={true} element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
